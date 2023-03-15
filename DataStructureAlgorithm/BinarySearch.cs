5a183d25b69768c17ed9d6e12582c31ed4660d23
BashaVali <bashavali222@gmail.com>
2023-03-15 09:04:48 +0530

    Binary Search

---
+++ DataStructureAlgorithm/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatatStructureAlgorithm
+{
+    public class BinarySearch
+    {
+        string[] binaryArray;
+        public void ReadTextFile(string filepath)
+        {
+            string words = File.ReadAllText(filepath);
+            binaryArray = words.Split(",");
+        }
+        public void BinarySearchOperation(string input)
+        {
+            foreach (var word in binaryArray)
+            {
+                if (word.Equals(input))
+                {
+                    Console.WriteLine("Found the Word in the List Using Binary Search");
+                    return;
+                }
+            }
+            Console.WriteLine("search element is not available in the list of words");
+        }
+    }
+}
---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,33 @@
+﻿using DatatStructureAlgorithm;
+using System;
+namespace DataStructureAlgorithm;
+class Program
+{
+    public static string binaryTextPath = @"C:\Users\Basha\Documents\BridgeLabz\DataStructureAlgorithm\DataStructureAlgorithm\BinarySearch.txt";
+    public static void Main(string[] args)
+    {
+        bool flag = true;
+        while (flag)
+        {
+            Console.WriteLine("\n\nWelcome to the Algorithm Programs " + "\n\n");
+
+            Console.WriteLine(" Select the option ");
+
+            Console.WriteLine("1.BinarySearch\n" +
+                              "2.Exit" + "\n");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    BinarySearch search = new BinarySearch();
+                    search.ReadTextFile(binaryTextPath);
+                    search.BinarySearchOperation("BinarySearch");
+                    break;
+
+                case 2:
+                    flag = false;
+                    break;
+            }
+        }
+    }
+}
