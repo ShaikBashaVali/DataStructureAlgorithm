a0e5f48b9e61811f3369f114f87b99b8acdd2eb0
BashaVali <bashavali222@gmail.com>
2023-03-15 09:09:35 +0530

    Insertion Sort

---
+++ DataStructureAlgorithm/InsertionSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class InsertionSort
+    {
+        // string[] binaryArr;
+        // public void ReadTextFile(string filepath)
+        //{
+        //  string words = File.ReadAllText(filepath);
+        //binaryArr = words.Split(",");       
+        //}
+        public void Sort(int[] binaryArr)
+        {
+            for (int i = 0; i < binaryArr.Length - 1; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (binaryArr[j].CompareTo(binaryArr[i]) < 0)
+                    {
+                        int temp = binaryArr[i];
+                        binaryArr[i] = binaryArr[j];
+                        binaryArr[j] = temp;
+                    }
+                }
+            }
+            foreach (var data in binaryArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        // public void Display()
+        //{
+        //  foreach (var data in binaryArr)
+        //{
+        //  Console.WriteLine(data);
+        //}
+    }
+}
--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -6,6 +6,7 @@ class Program
     public static string binaryTextPath = @"C:\Users\Basha\Documents\BridgeLabz\DataStructureAlgorithm\DataStructureAlgorithm\BinarySearch.txt";
     public static void Main(string[] args)
     {
+
         bool flag = true;
         while (flag)
         {
@@ -14,7 +15,8 @@ class Program
             Console.WriteLine(" Select the option ");
 
             Console.WriteLine("1.BinarySearch\n" +
-                              "2.Exit" + "\n");
+                              "2.InsertionSort\n" +
+                              "3.Exit" + "\n");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
             {
@@ -23,8 +25,13 @@ class Program
                     search.ReadTextFile(binaryTextPath);
                     search.BinarySearchOperation("BinarySearch");
                     break;
-
                 case 2:
+                    int[] arr = { 2, 1, 3 };
+                    InsertionSort insertion = new InsertionSort();
+                    insertion.Sort(arr);
+                    break;
+
+                case 3:
                     flag = false;
                     break;
             }
